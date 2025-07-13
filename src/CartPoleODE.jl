5d176402b3a265d7880743ff08dddf9926338d30
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2025-07-13 22:01:29 +0200

    damping in the cart

--- src/CartPoleODE.jl
+++ src/CartPoleODE.jl
@@ -6,11 +6,12 @@ const nx = 4    # number of states
 const nu = 1    # number of inputs
 
 struct Model
-    g    # ms⁻² - gravity
-    m_c  # kg - mass of the cart
-    m_p  # kg - mass of the point-mass 
-    l    # m - length of the pole
-    b_p 
+    g     # ms⁻² - gravity
+    m_c   # kg   - mass of the cart
+    m_p   # kg   - mass of the point-mass 
+    l     # m    - length of the pole
+    b_c   # Ns⁻¹ - damping coefficient for the cart
+    b_p   # Ns⁻¹ - damping coefficient for the pole
 end
 
 mass_matrix(m, x) = @SMatrix [
@@ -19,7 +20,7 @@ mass_matrix(m, x) = @SMatrix [
 ]
 
 torque_vector(m, x, u) = @SVector [
-    m.m_p * m.l * sin(x[2]) * x[4]^2 + u[1],
+    m.m_p * m.l * sin(x[2]) * x[4]^2 + u[1] - m.b_c * x[2],
     -m.g * m.m_p * m.l * sin(x[2]) - m.b_p * x[4]
 ]
 
